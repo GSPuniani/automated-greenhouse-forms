1d2e67c8a36fe747c17868843c8e86dccb72db89
Gobind Puniani <gobindpuniani@gmail.com>
2021-10-14 18:40:34 -0700

    User can save info to YAML file

--- script.py
+++ script.py
@@ -6,6 +6,7 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 # load_dotenv()
 # from decouple import config
 import os
+import yaml
 
 
 # Chromedriver
@@ -18,30 +19,42 @@ browser = webdriver.Chrome(chromedriver_location)
 
 # print(str(os.getenv('URL')))
 # browser.get(str(os.getenv('URL')))
-browser.get("https://boards.greenhouse.io/spacex/jobs/5479547002?gh_jid=5479547002&gh_src=e7cc0b212us#app")
+
+# Retrieve info from YAML file
+with open('config.yml', 'r') as file:
+    info = yaml.safe_load(file)
+
+# browser.get("https://boards.greenhouse.io/spacex/jobs/5479547002?gh_jid=5479547002&gh_src=e7cc0b212us#app")
+browser.get(info['url'])
 
 # First Name
 first_name = browser.find_element_by_xpath('//*[@id="first_name"]')
 # first_name.send_keys(os.environ.get('FIRST_NAME'))
 # first_name.send_keys(config('FIRST_NAME'))
-first_name.send_keys('Gobind')
+# first_name.send_keys('Gobind')
+first_name.send_keys(info['first_name'])
 
 # Last Name
 last_name = browser.find_element_by_xpath('//*[@id="last_name"]')
-last_name.send_keys('Puniani')
+# last_name.send_keys('Puniani')
+last_name.send_keys(info['last_name'])
 
 # Email
 email = browser.find_element_by_xpath('//*[@id="email"]')
-email.send_keys('gobindpuniani@gmail.com')
+# email.send_keys('gobindpuniani@gmail.com')
+email.send_keys(info['email'])
 
 # Phone
 phone = browser.find_element_by_xpath('//*[@id="phone"]')
-phone.send_keys('(559) 392-2944')
+# phone.send_keys('(559) 392-2944')
+phone.send_keys(info['phone'])
 
 # City
 city = browser.find_element_by_xpath('//*[@id="job_application_location"]')
-city.send_keys('Clovis')
+# city.send_keys('Clovis')
+city.send_keys(info['city'])
 
 # LinkedIn
 linkedin = browser.find_element_by_xpath('//*[@id="job_application_answers_attributes_15_text_value"]')
-linkedin.send_keys('https://www.linkedin.com/in/gobind-puniani/')
+# linkedin.send_keys('https://www.linkedin.com/in/gobind-puniani/')
+linkedin.send_keys(info['linkedin'])
